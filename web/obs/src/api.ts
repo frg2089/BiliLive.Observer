6112ad2d25f072ff54967f3e84de73900a54c4de
舰队的偶像-岛风酱! <frg2089@outlook.com>
2025-07-17 09:26:46 +0800

    路径检查器
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- web/obs/src/api.ts
+++ web/obs/src/api.ts
@@ -6,6 +6,14 @@ export const client = createClient<paths>({
   baseUrl: import.meta.env.VITE_BACKEND_BASE_URL,
 })
 
+/**
+ * 用于检查 API 合法性的工具
+ * @param path
+ * @returns
+ */
+export const pathChecker = (path: keyof paths | `${keyof paths}?${string}`) =>
+  path
+
 export const formDataBodySerializer = <T extends object>(data?: T) => {
   const form = new FormData()
   if (!data) return form
--- web/obs/src/components/Avatar.vue
+++ web/obs/src/components/Avatar.vue
@@ -1,6 +1,6 @@
 <template>
   <div relative w-16 h-16 flex items-center justify-center>
-    <img block w-9 h-9 rounded-full :src="`/bili/get?url=${encodeURIComponent(face)}`" />
+    <img block w-9 h-9 rounded-full :src="path(`/bili/get?url=${encodeURIComponent(face)}`)" />
     <img
       v-if="frame"
       absolute
@@ -10,14 +10,18 @@
       h-16
       block
       z-1
-      :src="`/bili/get?url=${encodeURIComponent(frame)}`" />
+      :src="path(`/bili/get?url=${encodeURIComponent(frame)}`)" />
   </div>
 </template>
 
 <script lang="ts" setup>
+import { pathChecker } from '../api';
+
 defineProps<{
   face: string
   frame?: string
 }>()
+
+const path = pathChecker
 
 </script>
