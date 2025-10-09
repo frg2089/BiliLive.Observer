6328029d02ad1d1538fb0138bcc6d725a35658bc
舰队的偶像-岛风酱! <frg2089@outlook.com>
2025-10-09 13:22:49 +0800

    移除没有用到的 using
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- src/BiliLive.Service/Program.cs
+++ src/BiliLive.Service/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System.Net;
-using System.Net.Http.Headers;
 using System.Net.Mime;
 using System.Net.NetworkInformation;
-using System.Text;
 using System.Text.Json;
 
 using BiliLive.Kernel;
